a65ca01bdb4937cf5c03a843dc6f99fb5fbf902d
VictoriaBorquez <victoria.borquez@usach.cl>
2023-05-11 14:57:21 -0400

    Add table in frontend with actions buttons

---
+++ frontend/reactapp/src/components/RestaurantTable.js
@@ -0,0 +1,73 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Button from '@mui/material/Button';
+
+
+export function RestaurantTable() {
+    const [restaurants, setRestaurants] = useState([]);
+
+
+
+    useEffect(() => {
+      async function fetchData() {
+        const response = await axios.get('http://0.0.0.0:8000/restaurants');
+        setRestaurants(response.data);
+      }
+      fetchData();
+    }, []);
+
+    return (
+        <TableContainer component={Paper}>
+            <h1>Tabla de Restaurantes</h1>
+            <Button variant="contained" startIcon={<AddCircleIcon />} color="success">
+                Agregar Restaurant
+            </Button>
+
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right">ID</TableCell>
+                        <TableCell align="right">Nombre Restaurante</TableCell>
+                        <TableCell align="right">Ubicación</TableCell>
+                        <TableCell align="right">Tipo de comida</TableCell>
+                        <TableCell align="right">Calificación</TableCell>
+                        <TableCell align="right">Visitado</TableCell>
+                        <TableCell align="right">Acciones</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {restaurants.map(restaurant => (
+                    <TableRow key={restaurant.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                        <TableCell align="right">{restaurant.id}</TableCell>
+                        <TableCell align="right">{restaurant.name}</TableCell>
+                        <TableCell align="right">{restaurant.location}</TableCell>
+                        <TableCell align="right">{restaurant.type_food}</TableCell>
+                        <TableCell align="right">{restaurant.calification}</TableCell>
+                        <TableCell align="right">{restaurant.visited ? <Checkbox checked={true} /> : <Checkbox checked={false}/>}</TableCell>
+                        <TableCell align="right">
+                            <IconButton aria-label="delete" size="large">
+                                <EditIcon fontSize="inherit" color="primary"/>
+                            </IconButton>
+                            <IconButton aria-label="delete" size="large">
+                                <DeleteIcon fontSize="inherit" color="error"/>
+                            </IconButton>
+                        </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
